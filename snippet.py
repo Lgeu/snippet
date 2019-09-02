593fee805cc4e2046fc8d45f9c3e51c5c46e852c
Lgeu <40058663+Lgeu@users.noreply.github.com>
2019-09-02 09:56:58 +0900

    update get_convex_hull

--- snippet.py
+++ snippet.py
@@ -520,38 +520,32 @@ class Dinic:
 
 
 # 凸包 Monotone Chain O(nlogn)
-def det(p, q):
-    return p[0]*q[1] - p[1]*q[0]
-def sub(p, q):
-    return (p[0]-q[0], p[1]-q[1])
+# 参考: https://matsu7874.hatenablog.com/entry/2018/12/17/025713
+# 複素数版は https://atcoder.jp/contests/abc139/submissions/7301049
 def get_convex_hull(points):
-    n = len(points)
+    def det(p, q):
+        return p[0] * q[1] - p[1] * q[0]
+    def sub(p, q):
+        return (p[0] - q[0], p[1] - q[1])
     points.sort()
-    size_convex_hull = 0
     ch = []
-    for i in range(n):
-        while size_convex_hull > 1:
+    for p in points:
+        while len(ch) > 1:
             v_cur = sub(ch[-1], ch[-2])
-            v_new = sub(points[i], ch[-2])
+            v_new = sub(p, ch[-2])
             if det(v_cur, v_new) > 0:
                 break
-            size_convex_hull -= 1
             ch.pop()
-        ch.append(points[i])
-        size_convex_hull += 1
-
-    t = size_convex_hull
-    for i in range(n-2, -1, -1):
-        while size_convex_hull > t:
+        ch.append(p)
+    t = len(ch)
+    for p in points[-2::-1]:
+        while len(ch) > t:
             v_cur = sub(ch[-1], ch[-2])
-            v_new = sub(points[i], ch[-2])
+            v_new = sub(p, ch[-2])
             if det(v_cur, v_new) > 0:
                 break
-            size_convex_hull -= 1
             ch.pop()
-        ch.append(points[i])
-        size_convex_hull += 1
-
+        ch.append(p)
     return ch[:-1]
 
 
