c70e0b5011d9a24f3c7672f7bb6c436d88ec9bd7
Lgeu <40058663+Lgeu@users.noreply.github.com>
2020-08-12 22:09:23 +0900

    update numba_compile

--- numba_library.py
+++ numba_library.py
@@ -1,32 +1,33 @@
 # 参考
 # https://ikatakos.com/pot/programming/python/packages/numba
 
-import os
-import sys
 import numpy as np
 
+
 # >>> numba compile >>>
-numba_config = [
-    [bit_sum, "i8(i8[:],i8)"],
-    [bit_add, "void(i8[:],i8,i8)"],
-    [inversion_number, "i8(f8[:])"],
-]
-if sys.argv[-1] == "ONLINE_JUDGE":
-    from numba import njit
-    from numba.pycc import CC
-    cc = CC("my_module")
-    for func, signature in numba_config:
-        vars()[func.__name__] = njit(signature)(func)
-        cc.export(func.__name__, signature)(func)
-    cc.compile()
-    exit()
-elif os.name == "posix":
-    exec(f"from my_module import {','.join(func.__name__ for func, _ in numba_config)}")
-else:
-    from numba import njit
-    for func, signature in numba_config:
-        vars()[func.__name__] = njit(signature, cache=True)(func)
-    print("compiled!", file=sys.stderr)
+def numba_compile(numba_config):
+    import os, sys
+    if sys.argv[-1] == "ONLINE_JUDGE":
+        from numba import njit
+        from numba.pycc import CC
+        cc = CC("my_module")
+        for func, signature in numba_config:
+            vars()[func.__name__] = njit(signature)(func)
+            cc.export(func.__name__, signature)(func)
+        cc.compile()
+        exit()
+    elif os.name == "posix":
+        exec(f"from my_module import {','.join(func.__name__ for func, _ in numba_config)}")
+        for func, _ in numba_config:
+            globals()[func.__name__] = vars()[func.__name__]
+    else:
+        from numba import njit
+        for func, signature in numba_config:
+            globals()[func.__name__] = njit(signature, cache=True)(func)
+        print("compiled!", file=sys.stderr)
+numba_compile([
+    [solve, "i8(i8,i8,i8[:,:])"],
+])
 # <<< numba compile <<<
 
 
